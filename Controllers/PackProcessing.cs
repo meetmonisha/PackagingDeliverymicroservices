6ed4bd54e100878d2a868bb13b9345e28f9c3623
Monisha <42087153+meetmonisha@users.noreply.github.com>
2022-08-22 16:01:45 +0530

    Add files via upload

---
+++ Controllers/PackProcessing.cs
@@ -0,0 +1,33 @@
+﻿using ChargeProcessing.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChargeProcessing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PackProcessing : ControllerBase
+    {
+        //------------------------------------------------------------------------
+        [HttpGet]
+        public IActionResult GetPackProcessing([FromQuery] GetDto data)
+        {
+
+            if (data.comType == "Integral")
+            {
+                data.packageCharges = 100 * data.quantity;
+                data.deliveryCharges = 200 * data.quantity;
+            }
+            else
+            {
+                data.packageCharges = 50 * data.quantity;
+                data.deliveryCharges = 100 * data.quantity;
+            }
+            return Ok(data);
+        }
+        //--------------------------------------------------------------------------------------
+    }
+}
